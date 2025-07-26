9c7e0b7851cb80345f9b9e5907a6aeab75f4d758
3000y <821923589@qq.com>
2025-07-26 14:17:54 +0800

    🐛 移除不必要的HTTP头
    移除了`Cache-Control`和`Content-Encoding`头，因为这些头在当前上下文中不再需要，可能会影响性能或导致不必要的缓存行为。

--- server/markdeep.go
+++ server/markdeep.go
@@ -8,8 +8,6 @@ import (
 func MarkDeep(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/javascript; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Cache-Control", "max-age=600")
-	w.Header().Set("Content-Encoding", "gzip")
 	w.Header().Set("Content-Length", fmt.Sprint(len(markDeepJS)))
 	w.Write([]byte(markDeepJS))
 }
